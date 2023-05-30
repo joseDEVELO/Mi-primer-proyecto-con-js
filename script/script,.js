4ccdfbf571ae21d0f2d09e55973a1dbf89f22afa
carlosllinas <soportehackcm@gmail.com>
2023-05-29 22:07:19 -0500

    29/05/23

--- script/script,.js
+++ script/script,.js
@@ -1,6 +1,6 @@
 const email = document.querySelector("#email");
-const btn = document.querySelector("button")
-const RegExp =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+const btn = document.querySelector("button");
+const RegExp =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
 
 btn.addEventListener("click", function(e){
