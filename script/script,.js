a7cd152581126cfa548f8e74cb7233f862216dab
carlosllinas <soportehackcm@gmail.com>
2023-05-29 22:08:13 -0500

    29/05/23

--- script/script,.js
+++ script/script,.js
@@ -1,11 +1,11 @@
 const email = document.querySelector("#email");
 const btn = document.querySelector("button");
-const RegExp =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+const Rg =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
 
 
 btn.addEventListener("click", function(e){
     e.preventDefault()
-    if (RegExp.test(email.value)) {
+    if (Rg.test(email.value)) {
         alert("Correo valido")
         console.log("click");
     }
