a9fd1e0530c5067f9c5d32168e95e44702126724
carlosllinas <soportehackcm@gmail.com>
2023-05-29 22:00:00 -0500

    Cambio final 29/05/23

---
+++ script/script,.js
@@ -0,0 +1,16 @@
+const email = document.querySelector("#email");
+const btn = document.querySelector("button")
+const RegExp =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/ 
+
+
+btn.addEventListener("click", function(e){
+    e.preventDefault()
+    if (RegExp.test(email.value)) {
+        alert("Correo valido")
+        console.log("click");
+    }
+     else {
+        alert("Correo invalido")
+     }
+})
+
